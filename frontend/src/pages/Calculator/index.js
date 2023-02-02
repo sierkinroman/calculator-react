914148bbdf0f53c0b0d950b3a4e885139cae649f
Roman Sierkin <serkin99@ukr.net>
2023-02-02 13:02:13 +0200

    refactor connection store

--- frontend/src/pages/Calculator/index.js
+++ frontend/src/pages/Calculator/index.js
@@ -1,1 +1,21 @@
-export { default } from './containers/Calculator';
+import React from 'react';
+import Calculator from "./containers/Calculator";
+import { applyMiddleware, createStore } from "redux";
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
+import examplesReducer from './reducers/examples';
+
+const store = createStore(
+  examplesReducer,
+  applyMiddleware(thunkMiddleware),
+);
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Calculator />
+    </Provider>
+  )
+};
+
+export default App;
